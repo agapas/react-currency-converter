36a3735b1c785bf935e9428a918e734407ca5220
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2020-09-20 19:38:25 +0100

    Updates

---
+++ precache-manifest.5591a9950546c7ef5770bfb6e33fff02.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57dfc9554045234f49723af1442c73c9",
+    "url": "/react-currency-converter/index.html"
+  },
+  {
+    "revision": "91d14c0ebce9bd93d7b8",
+    "url": "/react-currency-converter/static/css/2.550c5806.chunk.css"
+  },
+  {
+    "revision": "a5a73a48ddbbef727ccc",
+    "url": "/react-currency-converter/static/css/main.4a6f6260.chunk.css"
+  },
+  {
+    "revision": "91d14c0ebce9bd93d7b8",
+    "url": "/react-currency-converter/static/js/2.b6048f71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-currency-converter/static/js/2.b6048f71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a73a48ddbbef727ccc",
+    "url": "/react-currency-converter/static/js/main.65342444.chunk.js"
+  },
+  {
+    "revision": "383061d652ddffaff869",
+    "url": "/react-currency-converter/static/js/runtime-main.fe2c33c2.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-currency-converter/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-currency-converter/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-currency-converter/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-currency-converter/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-currency-converter/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
